6cef7d6777aa819bba4f9f1a0ce91170aba0f13e
rohitsinghabad <rohitsinghrajputabad@gmail.com>
2025-12-29 23:57:59 +0530

    Implement admin panel routing, enhance login functionality, and create dashboard with blog statistics

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -15,17 +18,20 @@ const Login = () => {
             <h1 className='text-3xl font-bold'><span className='text-primary'>Admin</span> Login</h1>
             <p className='font-light'>Enter your credentials to access the admin panel</p>
            </div>
-           <form onSubmit={handleSubmit}>
+           <form className="mt-6 w-full max-w-md space-y-5 rounded-xl bg-white p-6 shadow-lg">
+
             <div className='flex flex-col'>
               <label>Email </label>
-                <input type="email" required placeholder='your email id' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
+                <input onChange={e=> setEmail(e.target.value)} value={email}
+                 type="email" required placeholder='your email id' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
               
             </div>
              <div className='flex flex-col'>
               <label> Password </label>
-                <input type="password" required placeholder='your password' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
+                <input onChange={e=> setPassword(e.target.value)} value={password} type="password" required placeholder='your password' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
               
             </div>
+            <button type="submit" className='w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90'>Login</button>
            </form>
         </div>
       </div>
