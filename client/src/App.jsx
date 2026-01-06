c9e3580272e84caea8764bebadda7742de5161a1
rohitsinghabad <rohitsinghrajputabad@gmail.com>
2026-01-07 01:07:34 +0530

    Frontend part is done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import ListBlog from './pages/admin/ListBlog'
 import Comment from './pages/admin/Comment'
 import Dashboard from './pages/admin/Dashboard'
 import Login from './components/admin/Login'
+import 'quill/dist/quill.snow.css'
 
 const App = () => {
   return (
--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react'
-import { assets } from '../../assets/assets'
+import React, { useEffect, useRef, useState } from 'react'
+import { assets, blogCategories } from '../../assets/assets'
+import quill from 'quill'
 
 const AddBlog = () => {
+
+  const editorRef = useRef(null)
+  const quillRef = useRef(null)
 
   const [image, setImage] = useState(false);
   const [title, setTitle] = useState('');
@@ -9,12 +13,20 @@ const AddBlog = () => {
   const [category, setCategory] = useState('Startup');
   const [isPublished, setIsPublished] = useState(false);
 
+  const generateContent = async()=> {
+
+  }
   const onSubmitHandler = async (e) => {
     e.preventDefault();
   }
+  useEffect(()=> {
+    if(!quillRef.current && editorRef.current) {
+      quillRef.current = new quill(editorRef.current, {theme: 'snow'})
+    }
+  }, [])
 
   return (
-    <form onClick={onSubmitHandler} className='flex-1 bg-blur-50/50 text-gray-600 h-full overflow-scroll'>
+    <form onSubmit={onSubmitHandler} className='flex-1 bg-blur-50/50 text-gray-600 h-full overflow-scroll'>
       <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded'>
         <p>
           Upload thumbnail
@@ -23,6 +35,34 @@ const AddBlog = () => {
           <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt=""  className='mt-2 h-16 rounded cursor-pointer'/>
           <input onChange={(e)=> setImage(e.target.files[0])} type="file" id='image' hidden required/>
         </label>
+
+        <p className='mt-4 '>Blog title</p>
+        <input type="text" placeholder='Type Text' required className='w-full 
+        max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded' onChange={(e)=> setTitle(e.target.value)} value={title} />
+
+        <p className='mt-4 '>Sub title</p>
+        <input type="text" placeholder='Type Text' required className='w-full 
+        max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded' onChange={(e)=> setTitle(e.target.value)} value={subTitle} />
+
+        <p className='mt-4 '>Blog Description</p>
+        <div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative'>
+          <div ref={editorRef}></div>
+          <button type='button' onClick={generateContent} className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer'> Generate with AI
+          </button>
+        </div>
+        <p className='mt-4'> Blog category</p>
+        <select onChange={e => setCategory(e.target.value)} name="category" className='mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded'>
+          <option value="">Select Category</option>
+          {blogCategories.map((item, index)=> {
+            return <option key={index} value={item}>{item}</option>
+          })}
+        </select>
+
+        <div>
+          <p>Publish Now</p>
+          <input type="checkbox" checked={isPublished} className='scale-125 cursor-pointer' onChange={e=> setIsPublished(e.target.checked)}/>
+        </div>
+        <button type="submit" className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'> Add Blog </button>
       </div>
 
     </form>
