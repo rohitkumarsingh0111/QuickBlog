33af5a40d00c78bebf48ca41a8c974293eb2dccc
rohitsinghabad <rohitsinghrajputabad@gmail.com>
2026-01-08 02:39:41 +0530

    backend setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from './configs/db.js';
+
+const app= express();
+
+await connectDB()
+// MiddleWares
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=> res.send("API is Working"))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> {
+    console.log("Server is running on port" + PORT)
+})
