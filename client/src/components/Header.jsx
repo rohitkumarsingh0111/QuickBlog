7cffd2ecdbbc53173e20aecaad46d0c0087e773f
rohitsinghabad <rohitsinghrajputabad@gmail.com>
2026-01-13 10:52:27 +0530

    Implement search functionality in Header and filter blogs in BlogList

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
 
 const Header = () => {
+
+  const {setInput, input} = useAppContext()
+  const inputRef = useRef()
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setInput(inputRef.current.value)
+  }
+  const onClear = ()=> {
+    setInput('')
+    inputRef.current.value = ''
+  }
+
   return (
     <div className="mx-8 sm:mx-16 xl:mx-24 relative">
       <div className="text-center mt-20 mb-8">
@@ -19,11 +33,15 @@ const Header = () => {
           starts right here.
         </p>
 
-        <form className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
-          <input type="text" placeholder="Search for blogs" required className="w-full pl-4 outline-none"/>
+        <form onSubmit={onSubmitHandler} className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
+          <input ref= {inputRef} type="text" placeholder="Search for blogs" required className="w-full pl-4 outline-none"/>
           <button type="submit" className="bg-primary text-white px-8 py-2 m-1.5 rounded hover:scale-105 transition-all cursor-pointer">Search</button>
         </form>
 
+      </div>
+      <div className="text-center">{input &&
+        <button onClick={onClear} className="border font-light text-xs py-1 px-3 rounded-sm shadow-custom-sm cursor-pointer">Clear Search</button>
+      }
       </div>
       <img
         src={assets.gradientBackground}
